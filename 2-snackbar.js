379b4d7f67295a0ae6e4a65fe61014e824a3af78
npscxs <116647483+npscxs@users.noreply.github.com>
2025-01-23 08:41:30 +0000

    Deploying to gh-pages from @ npscxs/goit-js-hw-10@4e332dee227e4b3ce6b95e670c283941a68644e4 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector(".form");m.addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.delay.value),o=t.target.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
